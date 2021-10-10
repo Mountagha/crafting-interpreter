73d7f1a980a6b82d8931c0f71e844c27cf75d9bb
Mountagha <muntaghaba@gmail.com>
2021-10-09 23:55:37 -0400

    adding structure

---
+++ src/lox/lox.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // for debugging
+#include "lox.hpp"
+
+using namespace lox;
+
+void Lox::Run() {
+    std::cout << "Lox interpreter";
+}
---
+++ src/lox/lox.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace lox {
+
+class Lox {
+    public:
+        void Run();
+};
+
+}
