7533d91aa8af1b472025a5a0be7f87ce646b5b00
Mountagha <muntaghaba@gmail.com>
2021-10-12 22:26:04 -0400

    Scanner almost done..

--- src/scanner.hpp
+++ src/scanner.hpp
@@ -34,10 +34,49 @@ class Scanner {
                 case '+': addToken(PLUS); break;
                 case ';': addToken(SEMICOLON); break;
                 case '*': addToken(STAR); break;
+                // operator
+                case '!':
+                    addToken(match('=') ? BANG_EQUAL : BANG);
+                    break;
+                case '=':
+                    addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                    break;
+                case '<':
+                    addToken(match('=') ? LESS_EQUAL : LESS);
+                    break;
+                case '>':
+                    addToken(match('=') ? GREATER_EQUAL : GREATER);
+                    break;
+                case '/':
+                    if (match('/')) {
+                        // A comment goes until the end of the line
+                        while (peek() != '\n' && !isAtEnd()) advance();
+                    } else {
+                        addToken(SLASH);
+                    }
+                    break;
+                case ' ':
+                case '\r':
+                case '\t':
+                    // ignore whitespaces
+                    break;
+                case '\n':
+                    line++;
+                
+                // literals
+                case '"': string(); break;
                 default:
-                    Lox::error(line, "Unexpected character");
+                    if (isDigit(c)) {
+                        number();
+                    } else {
+                        Lox::error(line, "Unexpected character");
+                    }
                     break;
             }
+        }
+
+        inline bool isDigit (char c) const {
+            return c >= '0' && c <= '9';
         }
 
         inline char advance() const {
@@ -52,6 +91,53 @@ class Scanner {
             std::string text = source.substr(start, current-start+1);
             tokens.push_back(Token(token_type, text, literal, line));
         }
+
+        bool match(char expected) {
+            if (isAtEnd()) return false;
+            if (source.at(current) != expected) return false;
+            current++;
+            return true;
+        }
+
+        char peek() {
+            if (isAtEnd()) return '\0';
+            return source.at(current);
+        } 
+
+        void string() {
+            while(peek() != '"' && !isAtEnd()){
+                if(peek() == '\n') line++;
+                advance();
+            }
+
+            if(isAtEnd()) {
+                Lox::error(line, "Unterminted string.");
+                return;
+            }
+
+            // the closing ".
+            advance();
+
+            // trim the surrounding quotes
+            std::string value = source.substr(start+1, current-start);
+            addToken(STRING, value);
+        }
+
+        void number() {
+            while(isDigit(peek())) advance();
+
+            // look for a fractional part.
+            if (peek() == '.' && isDigit(peekNext())){
+                // consume the "."
+                advance();
+                while(isDigit(peek())) advance();
+            }
+
+            addToken(NUMBER, std::stod(source.substr(start, current-start+1)));
+
+        }
+
+
 
 };
 }
