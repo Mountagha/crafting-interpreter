bb1bb043e083d5d91b648b9eac89e4787ebfa458
Mountagha <muntaghaba@gmail.com>
2021-10-18 17:37:47 -0400

    to get latest updates

--- src/lox/lox.cpp
+++ src/lox/lox.cpp
@@ -1,8 +1,10 @@
 #include <iostream> // for debugging
 #include "lox.hpp"
 
-using namespace lox;
+namespace lox{
 
 void Lox::Run() {
     std::cout << "Lox interpreter";
 }
+
+} // namespace lox 
--- src/lox/lox.hpp
+++ src/lox/lox.hpp
@@ -7,4 +7,4 @@ class Lox {
         void Run();
 };
 
-}
+} // namespace lox
