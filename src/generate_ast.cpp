18d5fd4a1586236b631839931f633b1fcef66900
Mountagha <muntaghaba@gmail.com>
2021-11-09 22:14:02 -0500

    still on generating ast. Shame

--- src/generate_ast.cpp
+++ src/generate_ast.cpp
@@ -2,12 +2,21 @@
 #include <map>
 #include <vector>
 #include <fstream>
+#include <utility>
 
+auto split(std::string s, std::string sep) {
+    auto delimiter = s.find(sep);
+    if (delimiter != std::string::npos)
+        return std::make_pair(s.substr(0, delimiter), s.substr(delimiter));
+    return std::make_pair(s, "");
+}
+void define_type(std::ofstream& out, std::string basename, std::string classname, std::string fields) {
 
+} 
 void defineAST(
         std::string output_dir, 
-        std::string basename
-        /* std::map<std::string, std::vector<std::pair<std::string, std::string>>> */) {
+        std::string basename,
+        std::map<std::string,std::string> map) { 
     std::string path = output_dir + "/" + basename + ".cpp";
     std::ofstream out{path};
     if (!out) {
@@ -15,10 +24,17 @@ void defineAST(
         std::exit(65);
     }
     out << "#include <iostream>\n\n";
+    out << "namespace lox { namespace AST { \n";
     out << "class Express {\n";
     out << "\tprivate:\n";
     out << "\t\tint n;\n";
     out << "};\n";
+
+    for (auto& e: map) {
+        std::string classname = e.first;
+        std::string fiels = e.second;
+        define_type(out, basename, classname, fiels);
+    }
 }
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -26,7 +42,14 @@ int main(int argc, char *argv[]) {
         std::exit(64);
     }
     std::string output_dir = argv[1];
-    defineAST("AST", "Expr");
+    std::map<std::string, std::string> map {
+        {"Binary", "Expr left, Token operator, Expr right"},
+        {"Grouping", "Expr expression"},
+        {"Literal", "Object value"},
+        {"Unary", "Token operator, Expr right"} 
+    };
+
+    defineAST("AST", "Expr", map);
     //auto my_map = {"binary": }
     return 0;
 }
