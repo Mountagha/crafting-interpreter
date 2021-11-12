4e468efa136d9c8aa7d3c49286fe174f3dde0b47
Mountagha <muntaghaba@gmail.com>
2021-11-11 22:20:48 -0500

    gemeratomg AST almost done

--- src/AST/Expr.cpp
+++ src/AST/Expr.cpp
@@ -1,6 +1,50 @@
-#include <iostream>
+#include "token.hpp"
 
-class Express {
+namespace lox { namespace AST { 
+
+class Expr {};
+
+class Binary: public Expr {
+	public:
+		Binary(Expr left, Token operator, Expr right) {
+			left = left;
+			operator = operator;
+			right = right;
+		}
 	private:
-		int n;
+		Expr left;
+		Token operator;
+		Expr right;
 };
+
+class Grouping: public Expr {
+	public:
+		Grouping(Expr expression) {
+			expression = expression;
+		}
+	private:
+		Expr expression;
+};
+
+class Literal: public Expr {
+	public:
+		Literal(Object value) {
+			value = value;
+		}
+	private:
+		Object value;
+};
+
+class Unary: public Expr {
+	public:
+		Unary(Token operator, Expr right) {
+			operator = operator;
+			right = right;
+		}
+	private:
+		Token operator;
+		Expr right;
+};
+
+} // AST namespace
+} // lox namespace
--- src/generate_ast.cpp
+++ src/generate_ast.cpp
@@ -4,38 +4,65 @@
 #include <fstream>
 #include <utility>
 
-auto split(std::string s, std::string sep) {
-    auto delimiter = s.find(sep);
-    if (delimiter != std::string::npos)
-        return std::make_pair(s.substr(0, delimiter), s.substr(delimiter));
-    return std::make_pair(s, "");
-}
-void define_type(std::ofstream& out, std::string basename, std::string classname, std::string fields) {
+std::vector<std::string> split(std::string s, std::string sep) {
+    std::vector<std::string> splits;
+    size_t pos = 0, delimiter = 0;
+    do { 
+        delimiter = s.find(sep, pos); 
+        splits.push_back(s.substr(pos, delimiter-pos));
+        pos = delimiter + sep.length();
+    } while(delimiter != std::string::npos);
 
-} 
+    return splits;
+}
+
+void define_type(std::ofstream& out, std::string basename, std::string classname, std::string fieldList) {
+    out << "class " + classname + ": public " + basename + " {\n";
+    out << "\tpublic:\n";
+    // Constructor
+    out << "\t\t" + classname + "(" + fieldList + ") {\n";
+    //store parameters in fields
+    auto fields = split(fieldList, ", ");
+    for (const auto& field: fields) {
+        std::string name = split(field, " ")[1];
+        out << "\t\t\t" + name + " = " + name + ";\n";
+    }
+    out << "\t\t}\n";
+    // fields
+    out << "\tprivate:\n";
+    for (const auto& field: fields) {
+        out << "\t\t" + field + ";\n";
+    }
+    out << "};\n\n";
+    
+}
+
 void defineAST(
         std::string output_dir, 
         std::string basename,
-        std::map<std::string,std::string> map) { 
+        std::map<std::string,std::string> map
+    ) { 
+
     std::string path = output_dir + "/" + basename + ".cpp";
     std::ofstream out{path};
     if (!out) {
         std::cerr << "Unable to open the file for writing";
         std::exit(65);
     }
-    out << "#include <iostream>\n\n";
-    out << "namespace lox { namespace AST { \n";
-    out << "class Express {\n";
-    out << "\tprivate:\n";
-    out << "\t\tint n;\n";
-    out << "};\n";
-
+    out << "#include \"token.hpp\"\n\n";
+    out << "namespace lox { namespace AST { \n\n";
+    // define base class
+    out << "class " + basename + " {};\n\n";
     for (auto& e: map) {
         std::string classname = e.first;
         std::string fiels = e.second;
         define_type(out, basename, classname, fiels);
     }
+    out << "} // AST namespace\n";
+    out << "} // lox namespace";
+
 }
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         std::cerr << "Usage: generate_ast <output_dir>";
@@ -49,7 +76,6 @@ int main(int argc, char *argv[]) {
         {"Unary", "Token operator, Expr right"} 
     };
 
-    defineAST("AST", "Expr", map);
-    //auto my_map = {"binary": }
+    defineAST(output_dir, "Expr", map);
     return 0;
 }
