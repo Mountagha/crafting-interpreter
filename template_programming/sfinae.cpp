d548d9a3c02a5271e2db147f39a0b7cafeb3bb15
Mountagha <muntaghaba@gmail.com>
2022-05-18 09:49:42 -0400

    Metaprogramming: sfinae

---
+++ template_programming/sfinae.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+// number of elements in a raw array.
+template<typename T, unsigned N>
+std::size_t len(T(&)[N]) {
+    return N;
+}
+
+// number of elements for a type having size type:
+template<typename T>
+typename T::size_type len(T const& t) {
+    return t.size();
+}
+
+int main() {
+    int a[10];
+    std::cout << len(a);        // only len() for array matches
+    std::cout << len("tmp");  // only len for array matches
+    std::vector<int> v;
+    std::cout << len(v);      // only len() for a type with size_type matches.
+    int *p;
+    //std::cout << len(p); // ERROR: no matching function is found.
+    return 0;
+}
+
